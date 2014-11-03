2a32ba52f55d073f19a64e0933914434f2913475
kuang <chopperkuang@gmail.com>
2014-11-03 11:58:54 +0800

    $resource必须添加 '/' 不然，会自动添加angularjs的前缀

--- public/services/employee.js
+++ public/services/employee.js
@@ -1,6 +1,6 @@
 app.
     factory('Employee', function ($resource) {
-        return $resource('employee/:id', {id:'@empNo'}, {
+        return $resource('/employee/:id', null, {
             'query': {method: 'GET', isArray: false},
             'update': {method: 'PUT'}
         });
